b9e07f5025e548ba7177dbd3b2e3aafdb4282a4a
Josh <mrjoshuasperry@gmail.com>
2021-04-14 16:44:25 -0400

    Setup webpack, babel, eslint, typescript, and sass

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { hot } from "react-hot-loader";
+import "./App.scss";
+
+class App extends React.Component {
+    public render(): React.ReactNode {
+        return <></>;
+    }
+}
+
+export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + "/public/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
+module.exports = {
+    entry: "./src/components/index.tsx",
+    devtool: "inline-source-map",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader", "ts-loader"]
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".jsx", ".js"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        hotOnly: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin(), HTMLWebpackPluginConfig]
+};
