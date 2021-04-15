3079f29a496fb61c0508a9ffd9ff9f2bf2792a9b
Josh <mrjoshuasperry@gmail.com>
2021-04-14 20:25:01 -0400

    Add reset css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { hot } from "react-hot-loader";
 import "./App.scss";
+import "../sass/reset.scss";
 
 class App extends React.Component {
     public render(): React.ReactNode {
