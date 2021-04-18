6908e4b0261b08560e3cf7467bdb6d4e3b8cb103
Josh <mrjoshuasperry@gmail.com>
2021-04-18 10:29:58 -0400

    Add additional UX for the main profile page

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,19 +1,22 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { fadeInElements } from "../utils";
 import Headshot from "../../assets/images/profile/headshot.jpg";
 import Resume from "../../assets/resume.pdf";
 import "./Profile.scss";
 
 export default class Profile extends React.Component {
+    public componentDidMount(): void {
+        fadeInElements(document.getElementsByClassName("profile").item(0).children, 750);
+    }
+
     public render(): React.ReactNode {
         return (
             <section className="profile">
-                <div>
-                    <img alt="My head shot picture" src={Headshot}/>
-                    <h1>Hi, I'm Joshua Sperry</h1>
-                    <h2>I'm a front-end web developer and software developer</h2>
-                    <Link to="/portfolio" className="portfolio-link">View my portfolio</Link>
-                </div>
+                <img alt="My head shot picture" src={Headshot}/>
+                <h1>Hi, I'm Joshua Sperry</h1>
+                <h2>I'm a front-end web developer and software developer</h2>
+                <Link to="/portfolio" className="portfolio-link">View my portfolio</Link>
                 <footer>
                     <a target="_blank" href={Resume}>Resume</a>
                     <a title="mrjoshuasperry@gmail.com" href="mailto:mrjoshuasperry@gmail.com">Email</a>
